d1e53daf4105f8edf8ca81cba5cb9e5622baf284
Scott Techau <scotttechau@gmail.com>
2018-11-22 20:18:22 -0800

    Create some test specs for the CardPile

--- tests/components/cardPile.spec.js
+++ tests/components/cardPile.spec.js
@@ -5,26 +5,77 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { spy } from 'sinon';
 
-import { CardPile } from '../../src/components';
+import { CardPile, Card, CardList } from '../../src/components';
+import { testHand, testDeck } from '../testData';
 
 const adapter = new Adapter();
 Enzyme.configure({ adapter });
 
 describe('<CardPile /> Component', () => {
+  let wrapper;
   const drawSpy = spy();
 
+  beforeEach('Create component', () => {
+    wrapper = shallow(<CardPile cards={testHand} faceDown />);
+  });
+
   describe('Rendering', () => {
-    it('shows an empty pile with no cards');
-    it('shows the appropriate number of cards with a few');
-    it('only shows a max number of cards');
-    it('shows an indicator of number of cards in the pile');
-    it('propogates `faceDown` prop to all cards');
+    it('shows an empty pile with no cards', () => {
+      const empty = shallow(<CardPile cards={[]} />);
+      expect(empty.find(Card).length).to.equal(0);
+    });
+
+    it('shows the appropriate number of cards with a few', () => {
+      expect(wrapper.find(Card).length).to.equal(testHand.length);
+    });
+
+    it('only shows of 5 cards max number of cards', () => {
+      const deck = shallow(<CardPile cards={testDeck} />);
+      expect(deck.find(Card).length).to.equal(5);
+    });
+
+    it('shows an indicator of number of cards in the pile', () => {
+      expect(
+        wrapper.findWhere(n => n.text().contains(testHand.length))
+      ).to.have.property('length', 1);
+    });
+
+    it('propogates `faceDown` prop to all cards', () => {
+      wrapper.find(Card).forEach(card => {
+        expect(card.props()).to.have.property('faceDown', false);
+      });
+    });
+
+    it('properly shows when a scary card is on top', () => {
+      expect(
+        wrapper
+          .find(Card)
+          .first()
+          .hasClass('scary')
+      ).to.be.true;
+    });
   });
 
   describe('Functionality', () => {
     afterEach('Reset the spy', () => {
       drawSpy.resetHistory();
     });
-    it('Calls `drawCard` when the top card is clicked');
+
+    it('calls `drawCard` when the top card is clicked', () => {
+      expect(drawSpy).to.have.property('callCount', 0);
+      wrapper
+        .find(Card)
+        .first()
+        .simulate('click');
+      expect(drawSpy).to.have.property('callCount', 1);
+    });
+
+    it("doesn't crash if top card is click and `drawCard` isn't specified", () => {
+      const noClicking = shallow(<CardList cards={testHand} />);
+      noClicking
+        .find(Card)
+        .first()
+        .simulate('click');
+    });
   });
 });
--- tests/testData.js
+++ tests/testData.js
@@ -24,3 +24,77 @@ export const testHand = [
   },
   card
 ];
+
+export const testDeck = [
+  card,
+  {
+    id: 2,
+    title: 'Fake Card',
+    points: -10,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 3,
+    title: 'Fake Card 2',
+    points: 0,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 4,
+    title: 'Fake Card 3',
+    points: -10,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 5,
+    title: 'Fake Card 4',
+    points: 0,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 6,
+    title: 'Fake Card 5',
+    points: -10,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 7,
+    title: 'Fake Card 6',
+    points: 0,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 8,
+    title: 'Fake Card 7',
+    points: -10,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 9,
+    title: 'Fake Card 8',
+    points: 0,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 10,
+    title: 'Fake Card 9',
+    points: -10,
+    description: 'You win',
+    flavor: 'happiness?'
+  },
+  {
+    id: 11,
+    title: 'Fake Card 10',
+    points: 0,
+    description: 'You win',
+    flavor: 'happiness?'
+  }
+];
