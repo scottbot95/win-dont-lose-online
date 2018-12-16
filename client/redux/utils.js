3975ba8c2a0e2cdbadac64ed5cb35f71772ea428
Scott Techau <scotttechau@gmail.com>
2018-12-16 13:58:02 -0800

    Resolve #2.
    Function was re-queuing 'failed' run attempts by the runner,
    when it should have just run the runner again, without doing anything else.

--- client/redux/utils.js
+++ client/redux/utils.js
@@ -12,51 +12,57 @@ export const throttleAndQueueThunk = (thunkCreator, timeout = 0) => {
   const queue = [];
   let task = null;
 
+  const wrapper = genWrapper();
+  const wrapperNoQueue = genWrapper(false);
+
   // wrapper function
-  function wrapper() {
-    // calcuate remaining time difference
-    const now = new Date();
-    const diff = now - lastRun;
+  function genWrapper(addToQueue = true) {
+    return function() {
+      // calcuate remaining time difference
+      const now = new Date();
+      const diff = now - lastRun;
 
-    // eventual return value, default to an empty function
-    let result = emptyFunc;
+      // eventual return value, default to an empty function
+      let result = emptyFunc;
 
-    // if time is up, run it and move on with out lives
-    if (diff > timeout) {
-      lastRun = now;
-      result = thunkCreator(...arguments);
-    } else {
-      // trying to run too quick, add to queue
-      queue.push(arguments);
-      logging && console.log(`pushing ${[...arguments]} to queue`);
-    }
+      // if time is up, run it and move on with out lives
+      if (diff > timeout) {
+        lastRun = now;
+        result = thunkCreator(...arguments);
+      } else if (addToQueue) {
+        // trying to run too quick, add to queue
+        queue.push(arguments);
+        logging && console.log(`pushing ${[...arguments]} to queue`);
+      }
 
-    // if runner isn't scheduled, start one
-    if (queue.length && !task) {
-      const toRun = lastRun.getTime() - now.getTime() + timeout;
-      logging && console.log(`%cWaiting for ${toRun} more ms`, 'color: yellow');
-      task = setTimeout(() => {
-        logging && console.log('%ctimeout running', 'color:green');
-        // clear runner since we're running
-        task = null;
-        if (queue.length === 0)
-          return logging && console.warn('Runner has no tasks');
+      // if runner isn't scheduled, start one
+      if (queue.length && !task) {
+        const toRun = lastRun.getTime() - now.getTime() + timeout;
+        logging &&
+          console.log(`%cWaiting for ${toRun} more ms`, 'color: yellow');
+        task = setTimeout(() => {
+          logging && console.log('%ctimeout running', 'color:green');
+          // clear runner since we're running
+          task = null;
+          if (queue.length === 0)
+            return logging && console.warn('Runner has no tasks');
 
-        // peek the first element out of queue
-        const top = queue[0];
+          // peek the first element out of queue
+          const top = queue[0];
 
-        // dispatch the action (and possibly reschedule timeout)
-        const action = wrapper(...top);
-        if (action !== emptyFunc) {
-          store.dispatch(action);
-          queue.shift();
-        } else {
-          logging && console.log("%cdidn't wait long enough", 'color:red');
-        }
-      }, toRun);
-    }
+          // dispatch the action (and possibly reschedule timeout)
+          const action = wrapperNoQueue(...top);
+          if (action !== emptyFunc) {
+            store.dispatch(action);
+            queue.shift();
+          } else {
+            logging && console.log("%cdidn't wait long enough", 'color:red');
+          }
+        }, toRun);
+      }
 
-    return result;
+      return result;
+    };
   }
 
   return wrapper;
